4f08e81c777722909826a57a8aea38d7d0113ac8
madebyeggs <madebyeggs@gmail.com>
2016-03-09 11:42:29 +0000

    ajax loader for lazy load images

--- config/initializers/lazyload.rb
+++ config/initializers/lazyload.rb
@@ -1,3 +1,3 @@
 Lazyload::Rails.configure do |config|
-  config.placeholder = "https://dmffyspx7768r.cloudfront.net/black.png"
+  config.placeholder = "https://dmffyspx7768r.cloudfront.net/ajax-loader2.gif"
 end
