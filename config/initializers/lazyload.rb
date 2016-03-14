66d048a77cba7da7b0e0434697e984140ec2e9eb
madebyeggs <madebyeggs@gmail.com>
2016-03-14 23:52:17 +0000

    changes v1

--- config/initializers/lazyload.rb
+++ config/initializers/lazyload.rb
@@ -1,3 +1,3 @@
 Lazyload::Rails.configure do |config|
-  config.placeholder = "https://dmffyspx7768r.cloudfront.net/ajax-loader2.gif"
+  config.placeholder = "https://dmffyspx7768r.cloudfront.net/ajaxBig.gif"
 end
